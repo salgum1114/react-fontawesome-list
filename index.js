23e4fb8553dd514527d3afabbe183385ce05754f
Soroosh Mortazavi <37586118+SorooshMortazavi@users.noreply.github.com>
2021-07-25 16:01:35 +0430

    Update index.js
    this is for compatible react-fontawesome-list with nextjs because nextjs doesn`t support window object

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 if (!module.hot || process.env.NODE_ENV === 'production') {
-    module.exports = require('./dist/react-fontawesome-list.min.js');
+    if(process.browser){
+        module.exports = require('./dist/react-fontawesome-list.min.js');
+    }
 } else {
     module.exports = require('./dist/react-fontawesome-list.js');
 }
